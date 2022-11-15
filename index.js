7f156a4dc5b4362dfcdbd4ea7f18303d3e2f5723
subirdas29 <you@example.com>
2022-11-16 02:14:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+require('dotenv').config()
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1mua1t2.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+
+    try{
+        const appointmentOptionsCollection = client.db('doctorsPortal').collection('appointmentOption')
+
+        app.get('/appointmentOptions',async(req,res)=>
+        {
+            const query ={}
+            const options = await appointmentOptionsCollection.find(query).toArray()
+            res.send(options)
+        })
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
+
+app.get('/', async(req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, ()=>console.log(`Doctors portal running on ${port}`))
