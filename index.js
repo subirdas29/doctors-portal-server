51b789b3a17c4dee471f7c2879578f1a2cecb4e0
subirdas29 <dasjoy104@gmail.com>
2022-11-24 02:28:07 +0600

    delete,admin added

--- index.js
+++ index.js
@@ -40,6 +40,17 @@ async function run (){
         const usersCollection = client.db('doctorsPortal').collection('users')
         const doctorsCollection = client.db('doctorsPortal').collection('doctors')
 
+        const verifyAdmin = async(req,res,next)=>
+        {
+            const decodedEmail = req.decoded.email;
+            const query ={email:decodedEmail}
+            const user = await usersCollection.findOne(query)
+            if(user?.role!=='admin') {
+                return res.status(403).send({message:'forbidden access'})
+            }
+            next()
+        }
+
         app.get('/appointmentOptions',async(req,res)=>
         {
             const date = req.query.date;
@@ -136,14 +147,9 @@ async function run (){
             res.send({ isAdmin :user?.role ==='admin' })
         })
 
-        app.put('/users/admin/:id',verifyJwt ,async(req,res)=>
+        app.put('/users/admin/:id',verifyJwt,verifyAdmin, async(req,res)=>
         {
-            const decodedEmail = req.decoded.email;
-            const query ={email:decodedEmail}
-            const user = await usersCollection.findOne(query)
-            if(user?.role!=='admin') {
-                return res.status(403).send({message:'forbidden access'})
-            }
+           
            const id = req.params.id;
            const filter = ({_id:ObjectId(id)})
            const options = { upsert: true };
@@ -157,19 +163,26 @@ async function run (){
            res.send(result)
         })
 
-        app.post('/doctors',async(req,res)=>
-        {
-            const doctor = req.body
-            const result = await doctorsCollection.insertOne(doctor)
-            res.send(result)
+        app.get('/doctors',verifyJwt, verifyAdmin, async (req, res) => {
+            const query = {};
+            const doctors = await doctorsCollection.find(query).toArray();
+            res.send(doctors);
         })
 
-        app.get('/doctors',async(req,res)=>
-        {
-            const query = {}
-            const result = await doctorsCollection.find(query).toArray()
-            res.send(result)
+        app.post('/doctors',verifyJwt, verifyAdmin, async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorsCollection.insertOne(doctor);
+            res.send(result);
+        });
+
+        app.delete('/doctors/:id',verifyJwt, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await doctorsCollection.deleteOne(filter);
+            res.send(result);
         })
+
+       
 
     }
     finally{
